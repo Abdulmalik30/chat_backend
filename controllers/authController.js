634946a9b991f2543921a54cb5dd84a175e94443
Abiola <abdulmalikmuhiz01@gmail.com>
2023-10-03 15:56:52 +0100

    created handleLogin function

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 import { User } from '../model/userSchema.js';
-
+import jsonwebtoken from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 const handleRegister = async (req, res) => {
   const { username, password, email } = req.body;
 
@@ -9,19 +10,20 @@ const handleRegister = async (req, res) => {
       .json({ message: 'username, email and password are required' });
   }
 
-  const duplicate = await User.find({ email });
+  const duplicate = await User.findOne({ email });
 
   if (duplicate) {
     return res
       .status(409)
       .json({ message: 'user already exists, please login instead' });
   }
+  const hashedPwd = await bcrypt.hash(password, 10);
 
   try {
     const createdUser = await User.create({
       username,
       email,
-      password,
+      password: hashedPwd,
     });
   } catch (error) {
     console.log(error);
@@ -30,4 +32,29 @@ const handleRegister = async (req, res) => {
   res.status(201).json({ message: 'new user created' });
 };
 
-export { handleRegister };
+const handleLogin = async (req, res) => {
+  const jwt = jsonwebtoken;
+  const { email, password } = req.body;
+  if (!password || !email) {
+    return res.status(400).json({ message: 'email and password are required' });
+  }
+
+  const foundUser = await User.findOne({ email });
+  if (!foundUser) {
+    return res.status(401).json({ message: 'invalid email or password' });
+  }
+
+  const correctPwd = await bcrypt.compare(password, foundUser.password);
+
+  if (!correctPwd) {
+    return res.status(401).json({ message: 'invalid email or password' });
+  }
+
+  const accessToken = jwt.sign({ email }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: '2h',
+  });
+
+  res.json({ accessToken });
+};
+
+export { handleRegister, handleLogin };
